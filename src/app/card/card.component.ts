f6ed3e430cc587815307fdf79efb751b6069b038
David Bautista <dbautistav@gmail.com>
2019-01-09 18:07:47 -0600

    Add click handler to card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -20,10 +20,14 @@ export class CardComponent implements OnInit {
 
   /**
    * TODOs:
-   *   - click card header/body and open url!
    *   - finish 'link-list.component'
    *   - GET config.json, load it and process it
    *   - ? customize and save changes to local storage
    *   - ? export/import customizations
    */
+
+  //    TODO: finish this!
+  // see: vsavkin.com/angular-router-understanding-redirects-2826177761fc
+  // see: medium.com/@adrianfaciu/using-the-angular-router-to-navigate-to-external-links-15cc585b7b88  
+  openUrl() { }
 }
